b02708aa76c03265a1798cf34abc96e0faa293a6
john4650-hub <johndelvin51@gmail.com>
2023-01-27 17:08:50 +0300

    formatted code with black

--- app.py
+++ app.py
@@ -1,75 +1,86 @@
 #!/usr/bin/env python3
 from utils import *
+
 args = getArgs()
 try:
-	import pytube
-	from pytube import YouTube,Playlist
+    import pytube
+    from pytube import YouTube, Playlist
 except:
-	print('''
+    print(
+        """
     ⚠ ️Check if you have the pytube installed
 	⚠ Check if it's the latest version
-       ''')
-else:    
+       """
+    )
+else:
     print("Read the readme at github")
-    #https://youtu.be/tPEE9ZwTmy0
-    def main():        
-        print('\nYou are home 🏠, use --help to view usage')
+    # https://youtu.be/tPEE9ZwTmy0
+    def main():
+        print("\nYou are home 🏠, use --help to view usage")
         try:
             url = args.url
-            m = args.mode 
-            if url != 'done':
-                if m == '':
+            m = args.mode
+            if url != "done":
+                if m == "":
                     yt = YouTube(url)
                     vt = yt.title
-                    print(f'Video title: {vt}')
-                    print('\nAvailable video qualities')
+                    print(f"Video title: {vt}")
+                    print("\nAvailable video qualities")
                     video_quality(yt)
-                        
-                    audio_qualities = yt.streams.filter(only_audio = True)
-                    print('\nAvailable audio qualities')
+
+                    audio_qualities = yt.streams.filter(only_audio=True)
+                    print("\nAvailable audio qualities")
                     audio_quality(audio_qualities)
                     choice = args.itag
                     stm = yt.streams.get_by_itag(choice)
-                    
-                    print(f'Video saved in {stm.download()}\nDone.\n\n')
-                
-            # playlist
-                if m == 'pl':
+
+                    print(f"Video saved in {stm.download()}\nDone.\n\n")
+
+                # playlist
+                if m == "pl":
                     p = Playlist(url)
-                    interactive = input("\nEnter 'i' to prompt interactive mode or 'n' for none-interactive-mode: ")
+                    interactive = input(
+                        "\nEnter 'i' to prompt interactive mode or 'n' for none-interactive-mode: "
+                    )
                     count = len(p.videos)
-                    print(f'This playlist contains {count} videos')
-                    print('wait, calculating total size of this playlist')
-                    #print(f'Playlist total size: {totalsize(p.videos)}')		
-                    if interactive == 'i':
+                    print(f"This playlist contains {count} videos")
+                    print("wait, calculating total size of this playlist")
+                    # print(f'Playlist total size: {totalsize(p.videos)}')
+                    if interactive == "i":
                         for v in p.videos:
-                            print(f'\nTitle: {v.title}')
-                            print(f'Choose Video Quality')
+                            print(f"\nTitle: {v.title}")
+                            print(f"Choose Video Quality")
                             video_quality(v)
                             choice = args.itag
-            
-                            if choice == 'done':
-                                print('You are done with playlists, redirecting you back to home.')
+
+                            if choice == "done":
+                                print(
+                                    "You are done with playlists, redirecting you back to home."
+                                )
                                 break
-                                
-                            if choice == 'skip':
-                                print('skipping Video')
+
+                            if choice == "skip":
+                                print("skipping Video")
                                 continue
                             else:
                                 stm = v.streams.get_by_itag(int(choice))
-                                print('Download in progress...')
-                                print(f'Video saved in {stm.download()}\nDone.\n')
-                                
+                                print("Download in progress...")
+                                print(f"Video saved in {stm.download()}\nDone.\n")
+
                     else:
                         for v in p.videos:
-                            print(f'downloading: {v.title}')
-                            print(v.streams.first().download(),'done')		    
+                            print(f"downloading: {v.title}")
+                            print(v.streams.first().download(), "done")
         except:
-            print('''
+            print(
+                """
 ⚠ please check if you entered the correct link,
-⚠ Also check if you have internet connection on.''')
+⚠ Also check if you have internet connection on."""
+            )
         else:
-            print('\nDone \nThank you for using my program.\nHope you got you wanted ...')
+            print(
+                "\nDone \nThank you for using my program.\nHope you got you wanted ..."
+            )
 
     if __name__ == "__main__":
         main()
--- utils.py
+++ utils.py
@@ -1,41 +1,62 @@
 import argparse
 
+
 def getArgs():
-    parser = argparse.ArgumentParser(prog="app.py",description = "Download youtube videos easily")
-    parser.add_argument('url',metavar='videoUrl',help="Paste url of youtube video",default='done')
-    parser.add_argument('-m','--mode',metavar='mode',help='''
+    parser = argparse.ArgumentParser(
+        prog="app.py", description="Download youtube videos easily"
+    )
+    parser.add_argument(
+        "url", metavar="videoUrl", help="Paste url of youtube video", default="done"
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        metavar="mode",
+        help="""
     mode can either be pl->playlist 
-    or left empty->normal video''',default='')
-    parser.add_argument('-i','--itag',metavar='itag',type=int,help='''
+    or left empty->normal video""",
+        default="",
+    )
+    parser.add_argument(
+        "-i",
+        "--itag",
+        metavar="itag",
+        type=int,
+        help="""
     itag can only be number like
     17, 18, 22, ...Read the readme 
-    for supported itags ''',default=18)
-    
+    for supported itags """,
+        default=18,
+    )
+
     return parser.parse_args()
 
+
 def video_quality(video_object):
-	video_qualities = video_object.streams.filter(progressive= True)
-	for v in video_qualities:
-		m = str(v).split(' ')
-		y = m[1]
-		sizev = v.filesize * (9.5367431640625*10**-7)
-		vs = round(sizev,2)
-		print(f'  {y} ⇒ size: {(vs)}mb')
+    video_qualities = video_object.streams.filter(progressive=True)
+    for v in video_qualities:
+        m = str(v).split(" ")
+        y = m[1]
+        sizev = v.filesize * (9.5367431640625 * 10**-7)
+        vs = round(sizev, 2)
+        print(f"  {y} ⇒ size: {(vs)}mb")
+
 
 def audio_quality(audioQualityObject):
-	for aud in audioQualityObject:
-						m = str(aud).split(' ')
-						y = m[1]
-						sizev = aud.filesize * (9.5367431640625*10**-7)
-						vs = round(sizev,2)
-						
-						print(f'  {y} ⇒ size: {(vs)}mb')
+    for aud in audioQualityObject:
+        m = str(aud).split(" ")
+        y = m[1]
+        sizev = aud.filesize * (9.5367431640625 * 10**-7)
+        vs = round(sizev, 2)
+
+        print(f"  {y} ⇒ size: {(vs)}mb")
+
 
 def totalsize(pl):
-	total_size = []
-	for v in pl:
-		filt= v.streams.get_by_itag('18')
-		sizev = filt.filesize * (9.5367431640625*10**-7)
-		vs = round(sizev,2)
-		total_size.append(vs)
-	return str(sum(total_size))+'mbs'
+    total_size = []
+    for v in pl:
+        filt = v.streams.get_by_itag("18")
+        sizev = filt.filesize * (9.5367431640625 * 10**-7)
+        vs = round(sizev, 2)
+        total_size.append(vs)
+    return str(sum(total_size)) + "mbs"
