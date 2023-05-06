2ed9a5077ba272428489ea09c46da27a6e14ac55
john4650-hub <johndelvin51@gmail.com>
2023-05-06 03:15:09 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -1,7 +1,7 @@
 mkdir myMusic/
 pip install youtube-dl
 ls
-youtube-dl -F https://youtube.com/playlist?list=PL8JRTKSSfvuq_RvV4gIovCS0seiFynaQI
+youtube-dl -f mp4 https://youtube.com/playlist?list=PL8JRTKSSfvuq_RvV4gIovCS0seiFynaQI
 
 mv -f *.mp4 myMusic/
 zip -r myMusic.zip myMusic/
