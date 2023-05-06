9c49e5f057bbe967a99be32e5fb45983b80477b6
john4650-hub <johndelvin51@gmail.com>
2023-05-07 00:20:45 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -1,6 +1,6 @@
 mkdir myMusic/
 pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
 cd myMusic
-youtube-dl -f 18 --playlist-items 20-30 https://youtube.com/playlist?list=PLK3Jf2YakZU98E_ia8q2n2-XXpf8vH98W
+youtube-dl -f 18 --flat-playlist https://youtube.com/playlist?list=PL0o_zxa4K1BX8lFpIiKY97cHfKp7JP2NV 
 cd ..
 zip -r myMusic.zip myMusic/
