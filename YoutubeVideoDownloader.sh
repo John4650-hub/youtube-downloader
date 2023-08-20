abef9fddc064b915fec8119052e88cb3cb376efb
john4650-hub <johndelvin51@gmail.com>
2023-08-21 02:42:46 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -7,11 +7,11 @@ echo $vidName
 echo /////////////
 url=https://youtube.com/playlist?list=PL8JRTKSSfvuq_RvV4gIovCS0seiFynaQI
 # echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_' >> $GITHUB_ENV
-echo "[[[[[[[[[[[[[[{{{}}}]]]]]]]]]]]]]]"
+#
 echo "vidName=$(youtube-dl -J --flat-playlist "$url" | jq -r .title)"|tr ' ' '_' >> $GITHUB_ENV
 echo $vidname
 youtube-dl -f 18 --playlist-items 0-33 "$url"
-
+ls
 cd ..
 
 #youtube-dl -f 18 --playlist-items 0-33 https://youtube.com/playlist?list=PLK3Jf2YakZU98E_ia8q2n2-XXpf8vH98W 
