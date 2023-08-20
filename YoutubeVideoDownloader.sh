7d55c2980a0c83ca1cadc7eedbc12f04418eca02
john4650-hub <johndelvin51@gmail.com>
2023-08-20 08:24:22 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -8,6 +8,9 @@ echo /////////////
 url=https://youtu.be/tPEE9ZwTmy0
 
 echo "vidName=$(youtube-dl -f mp4 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)" >> $GITHUB_ENV
+cd ..
+mv myMusic $vidName
+ls
 echo /////////////
 echo $vidName
 echo /////////////
@@ -16,4 +19,4 @@ echo /////////////
 #youtube-dl -f 18 --playlist-items 0-33 https://youtube.com/playlist?list=PLK3Jf2YakZU98E_ia8q2n2-XXpf8vH98W 
 #youtube-dl -f 18 https://youtu.be/A1UDtFqDL48 
 cd ..
-zip -r myMusic.zip myMusic/
+zip -r ${vidName}.zip $vidName/
