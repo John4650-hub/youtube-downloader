c614d2f631896cbbc2a2da12e64e1c069149a14e
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2023-08-26 04:37:19 +0300

    Update YoutubeVideoDownloader.sh

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -5,7 +5,8 @@ cd myMusic
 echo /////////////
 echo $vidName
 echo /////////////
-url=https://youtube.com/playlist?list=PL8JRTKSSfvuq_RvV4gIovCS0seiFynaQI
+url=https://youtube.com/playlist?list=PL8JRTKSSfvuqweuPIlMQxUC3ax2V3XGTP
+# echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_' >> $GITHUB_ENV
 
 echo "vidName=$(youtube-dl -J --flat-playlist "$url" | jq -r .title)"|tr ' ' '_' >> $GITHUB_ENV
 echo $vidname
