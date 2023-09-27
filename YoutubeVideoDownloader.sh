f2fe09fb3dad595331329a1568289ea4c7a45a49
john4650-hub <johndelvin51@gmail.com>
2023-09-27 04:08:00 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -5,7 +5,7 @@ cd myMusic
 echo /////////////
 echo $vidName
 echo /////////////
-url="https://youtu.be/kICh_d6tHQk?si=HMN_fk7JqgQGH_kx"
+url="https://youtu.be/IzPEJ4kKhKY?si=EaNsUiQ7S8ecM386"
 echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_' >> $GITHUB_ENV
 
 #youtube-dl -f 18 "$url"
