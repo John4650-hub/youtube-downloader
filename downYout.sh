fb439e8337fc326cdbac51cbebf4dbea7072c85c
john4650-hub <johndelvin51@gmail.com>
2023-12-06 03:24:22 +0300

    videos

---
+++ downYout.sh
@@ -0,0 +1,14 @@
+USER=john4650-hub \
+REPO=youtube-downloader \
+GITHUB_API="https://api.github.com/repos/${USER}/${REPO}/releases/latest"
+token="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"
+LATEST_URL=$(curl -L \
+  -H 'Accept: application/json' \
+  -H "Authorization: Bearer $token"\
+  $GITHUB_API | jq -r ".assets[] | .url" | sed 's/\"//g')
+echo $LATEST_URL
+echo "Downloading ..." 
+curl -LC - \
+  -H 'Accept: application/octet-stream'\
+  -H "Authorization: Bearer $token"\
+ "${LATEST_URL}?access_token=${token}" -o vid.zip
