5b5d35ef5e134051d8d347d320b4852d5ef3977d
john4650-hub <johndelvin51@gmail.com>
2023-12-31 00:14:05 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -1,11 +1,11 @@
-vidName="vid"
+vidName="apis"
 mkdir myMusic/
 pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
 cd myMusic
 echo /////////////
 echo $vidName
 echo /////////////
-url="https://youtu.be/GDp854LJHaw?si=9Tbu1J7YeXUqfkIm"
+url="https://youtu.be/WXsD0ZgxjRw?si=5dlpkhU41PrjJfTJ"
 echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
 #echo "vidName=$(youtube-dl -x -o '%(id)s.%(ext)s' --print-json --no-warnings --audio-format mp3 "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_' > $GITHUB_ENV
 ls -lh
