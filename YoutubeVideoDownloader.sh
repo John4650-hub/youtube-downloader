4473f22f840d2765d1dd87dc4f54859a6c07fd33
john4650-hub <johndelvin51@gmail.com>
2024-01-07 09:34:57 +0300

    videos

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -5,12 +5,12 @@ cd myMusic
 echo /////////////
 echo $vidName
 echo /////////////
-url="https://youtu.be/A7PFDr9FPec"
-echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
+url="https://youtube.com/playlist?list=PLK3Jf2YakZU9d6j1hsXh7GmQUBesJeKVk&si=EyQThxfZp9jmaw3S"
+#echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
 #echo "vidName=$(youtube-dl -x -o '%(id)s.%(ext)s' --print-json --no-warnings --audio-format mp3 "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_' > $GITHUB_ENV
 ls -lh
-#youtube-dl -f 18 "$url"
-#echo "vidName=$(youtube-dl -J --flat-playlist "$url "| jq -r .title)"|tr ' ' '_'|tr '"' "_"|tr ':' '_' >> $GITHUB_ENV
+echo "vidName=$(youtube-dl -J --flat-playlist "$url"| jq -r .title)"|tr ' ' '_'|tr '"' "_"|tr ':' '_' >> $GITHUB_ENV
+youtube-dl --yes-playlist -f 18 "$url"
 echo $vidname
 ls
 
