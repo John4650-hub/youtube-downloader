4ef3b5ccc2cdd1b12ba827a879f64aff31975457
john4650-hub <johndelvin51@gmail.com>
2024-01-19 16:31:04 +0300

    gjdkjf

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -11,8 +11,11 @@ echo "Enter pl(for playlist) or  vd(for video): "
 fomat="$(cat ../fomat)"
 
 if [[ "$fomat"=="vd" ]]; then
-  echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
-  ffmpeg -i *.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "$GITHUB_ENV.mp4"
+  echo "vidName=$(youtube-dl -f 18 -o 'vid1.mp4' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
+  echo $GITHUB_ENV
+  ffmpeg -i vid1.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "$GITHUB_ENV.mp4"
+  rm vid1.mp4
+
 fi
 if [[ "$format" == "pl" ]]; then
   echo "vidName=$(youtube-dl -J --flat-playlist "$url"| jq -r .title)"|tr ' ' '_'|tr '"' "_"|tr ':' '_' >> $GITHUB_ENV
