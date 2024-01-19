093adfc5ee96e1a62545f5cdfd9a3ab56139264b
john4650-hub <johndelvin51@gmail.com>
2024-01-19 15:12:05 +0300

    All done

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -6,9 +6,9 @@ echo /////////////
 echo $vidName
 echo /////////////
 echo "enter url to video or playlist: "
-url="$(cat ./url__)"
+url="$(cat ../url__)"
 echo "Enter pl(for playlist) or  vd(for video): "
-fomat="$(cat ./fomat)"
+fomat="$(cat ../fomat)"
 
 if [[ "$fomat"=="vd" ]]; then
   echo "vidName=$(youtube-dl -f 18 -o '%(id)s.%(ext)s' --print-json --no-warnings "$url" | jq -r .title)"|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_' > $GITHUB_ENV
