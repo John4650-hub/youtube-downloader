4df1a93b13e77c9c2122bc50a45c7af729312fea
john4650-hub <johndelvin51@gmail.com>
2024-01-25 10:20:24 +0300

    bhnl

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -5,7 +5,12 @@ mkdir vid
 cd vid
 if [[ "$fomat"=="vd" ]]; then
   vname=$(youtube-dl -f 18 -o 'vid1.mp4' --print-json --no-warnings "$url" | jq -r .title)|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_'
+  echo "ooooooollooooooooooooooooooooooooooooo"
+  echo "ooooooollooooooooooooooooooooooooooooo"
+  echo "ooooooollooooooooooooooooooooooooooooo"
   echo $vname
+  echo "ooooooollooooooooooooooooooooooooooooo"
+  echo "ooooooollooooooooooooooooooooooooooooo"
   ffmpeg -i vid1.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "${vname}.mp4"
   ls -lh
   rm vid1.mp4
