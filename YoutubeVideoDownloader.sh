6284c40ce3120f144d83924c5a631c0a4a3f87ec
john4650-hub <johndelvin51@gmail.com>
2024-01-25 10:30:54 +0300

    gdif

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -4,19 +4,20 @@ fomat="$(cat fomat)"
 mkdir vid
 cd vid
 if [[ "$fomat"=="vd" ]]; then
-  vname=$(youtube-dl -f 18 -o 'vid1.mp4' --print-json --no-warnings "$url" | jq -r .title)|tr ' ' '_'|tr '|' '_'|tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_'
+  vname=$(youtube-dl -f 18 -o 'vid1.mp4' --print-json --no-warnings "$url" | jq -r .title)
+  vname=$(echo "$vname" | tr ' ' '_' | tr '|' '_' | tr ':' '_'|tr '*' '_'|tr '>' '_'|tr '<' '_')
   echo "ooooooollooooooooooooooooooooooooooooo"
   echo "ooooooollooooooooooooooooooooooooooooo"
   echo "ooooooollooooooooooooooooooooooooooooo"
   echo $vname
   echo "ooooooollooooooooooooooooooooooooooooo"
   echo "ooooooollooooooooooooooooooooooooooooo"
-  ffmpeg -i vid1.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "${vname}.mp4"
-  ls -lh
-  rm vid1.mp4
+  #ffmpeg -i vid1.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "${vname}.mp4"
+  #ls -lh
+  #rm vid1.mp4
 
 fi
-if [[ "$format" == "pl" ]]; then
+aif [[ "$format" == "pl" ]]; then
   echo "vidName=$(youtube-dl -J --flat-playlist "$url"| jq -r .title)"|tr ' ' '_'|tr '"' "_"|tr ':' '_' >> $GITHUB_ENV
   youtube-dl --yes-playlist -f 18 "$url"
 fi
