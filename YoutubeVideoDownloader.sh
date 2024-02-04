b91f869f0f80ad65e8cb93c044ddcedb122d978d
john4650-hub <johndelvin51@gmail.com>
2024-02-04 08:44:32 +0300

    gf

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -1,30 +1,38 @@
-pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
-url="$(cat url__)"
-fomat="$(cat fomat)"
-mkdir vid
-cd vid
-if [[ "$fomat"=="vd" ]]; then
-  echo "$(youtube-dl -f 18 -o 'vid1.mp4' --print-json --no-warnings "$url" | jq -r .title)">metadata
-  vidname=$(cat metadata)
-  vname=$(echo $vidname | tr ' ' '_' | tr '|' '_' | tr ':' '_' | tr '*' '_')
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  echo "$vname"
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  echo "ooooooollooooooooooooooooooooooooooooo"
-  ffmpeg -i vid1.mp4 -b:v 100k -c:a copy -c:v libx264 -c:a aac "${vname}.mp4"
-  ls -lh
-  rm vid1.mp4
+#!/bin/bash
 
-fi
-if [[ "$format" == "pl" ]]; then
-  echo "vidName=$(youtube-dl -J --flat-playlist "$url"| jq -r .title)"|tr ' ' '_'|tr '"' "_"|tr ':' '_' >> $GITHUB_ENV
-  youtube-dl --yes-playlist -f 18 "$url"
+# Install youtube-dl
+pip install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
+
+# Read URL and format from files
+url="$(cat url__)"
+format="$(cat format)" # Corrected variable name here
+
+# Create directory for videos and enter it
+mkdir -p vid
+cd vid
+
+if [[ "$format" == "vd" ]]; then
+    # Process for a single video
+    vid_info=$(youtube-dl -f best -o '%(title)s.%(ext)s' --print-json --no-warnings "$url")
+    vid_title=$(echo $vid_info | jq -r .title | tr '+|:*' '____')
+    vid_ext=$(echo $vid_info | jq -r .ext)
+
+    # Rename and convert the video
+    ffmpeg -i "${vid_title}.${vid_ext}" -b:v 100k -c:a aac -c:v libx264 "${vid_title}_formatted.mp4"
+    rm "${vid_title}.${vid_ext}"
+elif [[ "$format" == "pl" ]]; then
+    # Process for a playlist
+    youtube-dl -f best -o '%(title)s.%(ext)s' --yes-playlist "$url"
+    for file in *; do
+        if [[ -f "$file" ]]; then
+            base_name=$(basename "$file" .${file##*.})
+            # Ensure the filename is safe
+            safe_name=$(echo $base_name | tr '+|:*' '____')
+            # Convert and rename the video
+            ffmpeg -i "$file" -b:v 100k -c:a aac -c:v libx264 "${safe_name}_formatted.mp4"
+            rm "$file"
+        fi
+    done
 fi
 
 cd ..
-#mv myMusic "$vidName"
-#ls
-#zip -r "$vidName".zip "$vidName"/
