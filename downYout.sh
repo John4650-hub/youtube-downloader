2f879e53701c0f47f414d9f89b9d40772864e28e
john4650-hub <johndelvin51@gmail.com>
2024-04-14 06:04:02 +0300

    Download video

--- downYout.sh
+++ downYout.sh
@@ -1,6 +1,6 @@
 USER=john4650-hub \
 REPO=youtube-downloader \
-TAG="340.0.0"
+TAG="341.0.0"
 GITHUB_API="https://api.github.com/repos/${USER}/${REPO}/releases/tags/${TAG}"
 token="$(cat ../.secrets/git_token)"
 echo $token
