2762bfe4791a2d2372dcc4d64ac76f0d7dc4aad2
john4650-hub <johndelvin51@gmail.com>
2024-06-23 04:00:16 +0300

    Download video

--- downYout.sh
+++ downYout.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 USER=john4650-hub
 REPO=youtube-downloader
-for i in {5..6}; do
-  TAG="426.0.0"
-  echo $TAG
-  GITHUB_API="https://api.github.com/repos/${USER}/${REPO}/releases/tags/${TAG}"
-  token="$(cat ../.secrets/git_token)"
-  echo $token
-  LATEST_URL=$(curl -L \
-    -H 'Accept: application/json' \
-    -H "Authorization: Bearer $token"\
-    $GITHUB_API | jq -r ".assets[] | .browser_download_url" | sed 's/\"//g')
-  echo $LATEST_URL
-  echo "Downloading ..." 
-  cd /storage/3461-6461/Android/media/com.termux/vid
-  curl -L \
-    -H 'Accept: application/octet-stream'\
-    -H "Authorization: Bearer $token"\
-   "${LATEST_URL}" -o "vid${TAG}.zip"
-  cd -
-done
+TAG="426.0.0"
+echo $TAG
+GITHUB_API="https://api.github.com/repos/${USER}/${REPO}/releases/tags/${TAG}"
+token="$(cat ../.secrets/git_token)"
+echo $token
+LATEST_URL=$(curl -L \
+  -H 'Accept: application/json' \
+  -H "Authorization: Bearer $token"\
+  $GITHUB_API | jq -r ".assets[] | .browser_download_url" | sed 's/\"//g')
+echo $LATEST_URL
+echo "Downloading ..." 
+cd /storage/3461-6461/Android/media/com.termux/vid
+curl -L \
+  -H 'Accept: application/octet-stream'\
+  -H "Authorization: Bearer $token"\
+ "${LATEST_URL}" -o "vid${TAG}.zip"
+cd -
