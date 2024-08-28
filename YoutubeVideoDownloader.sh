9b83081e373f97a14da947501504555688b23023
john4650-hub <johndelvin51@gmail.com>
2024-08-28 19:42:01 +0300

    Download video

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -5,7 +5,7 @@ cd vid
 mv ../rename.py ../main.py ../info.txt ../url.txt .
 python main.py
 vid_title= echo $(cat info.txt)
-url= echo $(cat url.txt)
+url=$(echo $(cat url.txt))
 echo $url
 ffmpeg -i "${url}" -b:v 100k -c:a aac -c:v libx264 "${vid_title}.mp4"
 python rename.py
