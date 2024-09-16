4528c5efd67913c06d19103213e62ea0e3f80c02
john4650-hub <johndelvin51@gmail.com>
2024-09-16 19:41:54 +0300

    Download video

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -8,7 +8,7 @@ vid_title=$(echo $(cat info.txt))
 url=$(echo $(cat url.txt))
 echo $url
 #ffmpeg -i "${url}" -b:v 100k -c:a aac -c:v libx264 "${vid_title}.mp4"
-ffmpeg -i "https://cdn23.camhub.world/remote_control.php?time=1726228802&cv=23e0d2b5c6f4a7bb7e6babd42d7ad2b6&lr=0&cv2=e3500c3a495217ace246931354fa1082&file=%2F548000%2F548494%2F548494.mp4&cv3=501513718379faa208eee771e90ffe3e&cv4=91d9df74e480a0e7c4ab1bff0fca8935" -b:v 100k -c:a aac -c:v libx264 "leena.mp4"
+ffmpeg -i "https://backup.blackporn24.com/remote_control.php?time=1726504840&cv=024ba65ecf32560b0f7bd40e1570096a&lr=0&cv2=65573b77279b13d46bc3e706135dcd03&file=%2Fvideos%2F5000%2F5184%2F5184.mp4&cv3=06acca556021536eb8d13c95b0e4bf6a&cv4=29f45b3a064e65bd67ae10bf1349b755" -b:v 100k -c:a aac -c:v libx264 "leena.mp4"
 #ffmpeg -i "${url}" -vf "scale=1280:720" -b:v 100k -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 64k "${vid_title}.mp4"
 python rename.py
 
