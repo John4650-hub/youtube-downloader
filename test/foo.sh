00346a89bc11bc3d0df04d611cdd32dd8631339b
john4650-hub <johndelvin51@gmail.com>
2024-09-23 17:32:54 +0300

    Download video

--- test/foo.sh
+++ test/foo.sh
@@ -8,9 +8,8 @@ url=$(< url.txt)
 #python rd.py
 
 # Read and validate `tm` from JSON, assuming foo.json contains a simple number
-cat foo.json
-tm=$(< foo.json)
-echo "////$tm//////////"
+tm=$(cat foo.json)
+tm="55"
 
 # Check if `tm` is a number within the range 0 to 840
 if ! [[ "$tm" =~ ^[0-9]{1,3}$ ]] || [[ "$tm" -lt 0 ]] || [[ "$tm" -gt 840 ]]; then
