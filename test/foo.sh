33aed77f90200165398148518f81fc32b26b1738
john4650-hub <johndelvin51@gmail.com>
2024-09-23 17:29:57 +0300

    Download video

--- test/foo.sh
+++ test/foo.sh
@@ -8,11 +8,12 @@ url=$(< url.txt)
 #python rd.py
 
 # Read and validate `tm` from JSON, assuming foo.json contains a simple number
+cat foo.json
 tm=$(< foo.json)
 echo "////$tm//////////"
 
 # Check if `tm` is a number within the range 0 to 840
-if ! [[ "$tm" =~ ^[0-9]{1,3}$ ]] || [ "$tm" -lt 0 ] || [ "$tm" -gt 840 ]; then
+if ! [[ "$tm" =~ ^[0-9]{1,3}$ ]] || [[ "$tm" -lt 0 ]] || [[ "$tm" -gt 840 ]]; then
   echo "Error: tm in foo.json is not a valid number between 0 and 840."
   exit 1
 fi
