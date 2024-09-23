c88b2725fe21c29b8e4b92cf057afa3b7b97e5ea
john4650-hub <johndelvin51@gmail.com>
2024-09-23 10:39:21 +0300

    Download video

--- test/foo.sh
+++ test/foo.sh
@@ -6,6 +6,6 @@ cd vid
 #ffmpeg -i "${url}" -b:v 100k -c:a aac -c:v libx264 "${vid_title}.mp4"
 #timeout 10s ffmpeg
 #ffmpeg -i "$url" -vframes 1 "${vid_title}.jpg"
-ffmpeg -ss 00:05:00 -i "$url" -frames:v 1 output.jpg
+ffmpeg -ss 00:10:00 -i "$url" -frames:v 1 output.jpg
 #ffmpeg -i "${url}" -vf "scale=1280:720" -b:v 100k -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 64k "${vid_title}.mp4"
 cd ..
