ea091ff2f1ec584ce0b0f3abf1cae52d1bca4b5c
john4650-hub <johndelvin51@gmail.com>
2024-10-06 17:48:25 +0300

    Download video

--- foo.sh
+++ foo.sh
@@ -10,7 +10,7 @@ python rd.py
 
 # Read and validate `tm` from JSON, assuming foo.json contains a simple number
 tm=$(< foo.json)
-
+stp=$(expr $tm-1)
 # Check if `tm` is a number within the range 0 to 840
 if ! [[ "$tm" =~ ^[0-9]{1,3}$ ]] || [[ "$tm" -lt 0 ]] || [[ "$tm" -gt 840 ]]; then
   echo "Error: tm in foo.json is not a valid number between 0 and 840."
@@ -22,7 +22,7 @@ mkdir -p vid
 cd vid || exit
 
 # Extract a frame starting at tm minutes
-ffmpeg -ss $((tm * 60)) -i "$url" -frames:v 1 output.jpg
+ffmpeg -ss $60)) -i "$url" -frames:v 1 output.jpg
 
 # Increment `tm` by 5 and save it back to foo.json
 echo $((tm + 5)) > ../foo.json
