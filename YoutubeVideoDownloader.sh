ba2d6da2f575d276a1599b9d43e41e784b75976d
john4650-hub <johndelvin51@gmail.com>
2024-10-15 23:13:09 +0300

    Download video

--- YoutubeVideoDownloader.sh
+++ YoutubeVideoDownloader.sh
@@ -7,7 +7,7 @@ mv ../rename.py ../main.py ../info.txt ../url.txt .
 vid_title=$(echo $(cat info.txt))
 url=$(echo $(cat url.txt))
 
-ffmpeg -i "${url}" -vf "scale=1280:720" -b:v 100k -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 64k "${vid_title}.mp4"
+ffmpeg -i "${url}" -vf "scale=360:500" -b:v 100k -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 64k "${vid_title}.mp4"
 python rename.py
 
 cd ..
